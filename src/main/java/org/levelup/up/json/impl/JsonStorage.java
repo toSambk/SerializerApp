614e13bff8a15f56158697dd0c586df9ed5534b3
Sam <bk.simon50@gmail.com>
2019-04-17 15:48:54 +0300

    init commit

---
+++ src/main/java/org/levelup/up/json/impl/JsonStorage.java
@@ -0,0 +1,66 @@
+package org.levelup.up.json.impl;
+
+import org.levelup.up.json.JsonSerializer;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class JsonStorage {
+
+    static Map <String, Object> SerializedMap;
+
+    static {
+        try {
+            loadClasses();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void loadClasses() throws IOException {
+        Collection <Class<?>> classes = scanDirectory();
+        int i = 3;
+        SerializedMap = classes.stream()
+                .filter(clazz -> {
+                    Class<?>[] interfaces = clazz.getInterfaces();
+                    return Arrays.stream(interfaces)
+                            .anyMatch(c->c== JsonSerializer.class);
+                })
+                .collect(Collectors.toMap (clazz -> clazz.getName(), clazz-> newInstance(clazz)));
+    }
+
+    private static Collection <Class<?>> scanDirectory() throws IOException {
+       return Files.walk(Paths.get("src\\main\\java\\org\\level\\up\\json"))
+        .map(Path::toFile)
+        .filter(File::isFile)
+        .map(File::toString)
+        .map(filename -> filename .replace("src\\main\\java\\", "").replace("/", "").replace("\\","").replace(".java",""))
+        .map(JsonStorage::loadClass)
+        .collect(Collectors.toList());
+    }
+
+
+    private static Class<?> loadClass (String fullName) {
+        try {
+            return Class.forName(fullName);
+        } catch (ClassNotFoundException exc) {
+            throw new RuntimeException(exc);
+        }
+    }
+
+    private static JsonSerializer<?> newInstance (Class <?> clazz) {
+        try {
+            return (JsonSerializer) clazz.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
