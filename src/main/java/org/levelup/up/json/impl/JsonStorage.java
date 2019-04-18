78b602b77e0f949482e802f4aced7985a99bfc53
Sam <bk.simon50@gmail.com>
2019-04-18 14:05:52 +0300

    Homework - reading from file added - making a collection of classes implemented Deserializer interface added

--- src/main/java/org/levelup/up/json/impl/JsonStorage.java
+++ src/main/java/org/levelup/up/json/impl/JsonStorage.java
@@ -1,9 +1,9 @@
 package org.levelup.up.json.impl;
 
+import org.levelup.up.json.JsonDeserializer;
 import org.levelup.up.json.JsonSerializer;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,7 +16,7 @@ public class JsonStorage {
 
     static Map <String, Object> SerializedMap;
 
-    private static int testcommit;
+    static Map <String, Object> DeserializedMap;
 
     static {
         try {
@@ -24,6 +24,10 @@ public class JsonStorage {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+            Thread.sleep(1000);
     }
 
     private static void loadClasses() throws IOException {
@@ -35,18 +39,35 @@ public class JsonStorage {
                             .anyMatch(c->c== JsonSerializer.class);
                 })
                 .collect(Collectors.toMap (clazz -> clazz.getName(), clazz-> newInstance(clazz)));
+
+        DeserializedMap = classes.stream()
+                .filter(clazz -> {
+                    Class<?>[] interfaces = clazz.getInterfaces();
+                    return Arrays.stream(interfaces)
+                            .anyMatch( c -> c == JsonDeserializer.class);
+                })
+                .collect(Collectors.toMap(clazz -> clazz.getName(), clazz -> newInstance(clazz)));
+
     }
 
     private static Collection <Class<?>> scanDirectory() throws IOException {
+
+
+        String dirline = null;
+        try (FileInputStream fin = new FileInputStream("C:\\Users\\Sam\\IdeaProjects\\jsonup\\dirname\\directory.txt")) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(fin));
+            dirline = br.readLine();
+        }
+
        return Files.walk(Paths.get("src\\main\\java\\org\\level\\up\\json"))
         .map(Path::toFile)
         .filter(File::isFile)
         .map(File::toString)
         .map(filename -> filename .replace("src\\main\\java\\", "").replace("/", "").replace("\\","").replace(".java",""))
         .map(JsonStorage::loadClass)
-        .collect(Collectors.toList());
+        .collect(Collectors.toList())
+               ;
     }
-
 
     private static Class<?> loadClass (String fullName) {
         try {
